3f091dae9dd426829668f84361bce5ffa61fb6eb
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2024-06-10 12:34:34 +0700

    feat: Adding product model and testing

---
+++ src/main/java/izzal/dev/pos/entity/Product.java
@@ -0,0 +1,37 @@
+package izzal.dev.pos.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data @NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+
+    public Product(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    void setId(Long id) {
+        if (this.id == null) {
+            this.id = id;
+        } else {
+            throw new IllegalStateException("ID can only be set once.");
+        }
+    }
+}
---
+++ src/test/java/izzal/dev/pos/entity/ProductTest.java
@@ -0,0 +1,42 @@
+package izzal.dev.pos.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductTest {
+    @Test
+    public void testProductCreation() {
+        // Arrange
+        Long id = 1L;
+        String name = "Test Product";
+        String description = "This is a test product";
+        int price = 10;
+
+        // Act
+        Product product1 = new Product();
+        product1.setId(id);
+        product1.setName(name);
+        product1.setDescription(description);
+        product1.setPrice(price);
+
+        Product product2 = new Product(name, description, price);
+        product2.setId(id);
+        
+        // Assert
+        assertEquals(id, product1.getId());
+        assertEquals(name, product1.getName());
+        assertEquals(description, product1.getDescription());
+        assertEquals(price, product1.getPrice(), 0.01);
+        assertThrows(IllegalStateException.class, () -> product1.setId(id));
+        assertEquals(id, product2.getId());
+        assertEquals(name, product2.getName());
+        assertEquals(description, product2.getDescription());
+        assertEquals(price, product2.getPrice(), 0.01);
+        assertThrows(IllegalStateException.class, () -> product2.setId(id));
+    }
+}
