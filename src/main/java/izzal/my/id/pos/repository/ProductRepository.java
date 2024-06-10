73b5b08323f8abe06d214e46c0ccacf960476bbe
Rizal Fadlullah <izzal@MacBook-Air-Rizal.local>
2024-06-10 09:34:53 +0700

    fix spring error

--- src/main/java/izzal/my/id/pos/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package izzal.my.id.pos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import izzal.my.id.pos.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    
-}
